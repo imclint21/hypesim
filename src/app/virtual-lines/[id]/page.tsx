9c8775615b4bca10959a3e1cb2e5f9f278c3cec5
clint21.eth ⚡️ <me@clint21.dev>
2025-06-16 21:04:35 +0200

    Add app clip

---
+++ src/app/virtual-lines/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	other: {
+		"apple-itunes-app": "app-id=id6746370915, app-clip-bundle-id=business.xxi.hypesim.Clip"
+	}
+};
+
+// { params }: { params: { code: string } }
+export default function Page () {
+	return <h1>Virtual Line</h1>
+}
