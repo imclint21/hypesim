8c5d0d1463656a70e650d9773cd248fe128e23e6
clint21.eth ⚡️ <me@clint21.dev>
2025-06-21 19:55:47 +0200

    Changing API base

--- src/app/lib.ts
+++ src/app/lib.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "https://localhost:7001";
+const BASE_URL = "https://api.hypesim.io";
 
 export async function getVirtualLine (id: string) {
 	const res = await fetch(`${BASE_URL}/virtual-lines/${id}/share`);
