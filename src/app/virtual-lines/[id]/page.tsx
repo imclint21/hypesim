886a55a46697fc5c91d2f3e0168e43a5090feb1c
clint21.eth ⚡️ <me@clint21.dev>
2025-06-21 20:00:07 +0200

    Adding theme-color

--- src/app/virtual-lines/[id]/page.tsx
+++ src/app/virtual-lines/[id]/page.tsx
@@ -3,7 +3,8 @@ import { Metadata } from "next";
 type Params = Promise<{ id: string }>;
 export const metadata: Metadata = {
 	other: {
-		"apple-itunes-app": "app-id=id6746370915, app-clip-bundle-id=business.xxi.hypesim.Clip"
+		"apple-itunes-app": "app-id=id6746370915, app-clip-bundle-id=business.xxi.hypesim.Clip",
+		"theme-color": "#FF6214"
 	}
 }
 
