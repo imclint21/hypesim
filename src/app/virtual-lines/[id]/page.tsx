e20ca945eb0efb284a1215da07b38cc0a3996c8b
clint21.eth ⚡️ <me@clint21.dev>
2025-06-21 19:45:46 +0200

    Try to fix https://vercel.com/clint21/hypesim/HUmJV5uCghuZVGgrgGg5bqDtm8j2?filter=errors

--- src/app/virtual-lines/[id]/page.tsx
+++ src/app/virtual-lines/[id]/page.tsx
@@ -1,24 +1,29 @@
 import { Metadata } from "next";
-import { getVirtualLine } from "@/app/lib";
 
 type Props = {
 	params: { id: string }
 }
 
-export async function generateMetadata ({ params }: Props): Promise<Metadata> {
-	const id = params.id
-	const virtualLine = await getVirtualLine(id);
-
-	return {
-		title: virtualLine.plan.name,
-		// openGraph: {
-		// 	images: [post.location.cover]
-		// },
-		other: {
-			"apple-itunes-app": "app-id=id6746370915, app-clip-bundle-id=business.xxi.hypesim.Clip"
-		}
+export const metadata: Metadata = {
+	other: {
+		"apple-itunes-app": "app-id=id6746370915, app-clip-bundle-id=business.xxi.hypesim.Clip"
 	}
 }
+
+// export async function generateMetadata ({ params }: Props): Promise<Metadata> {
+// 	const id = params.id
+// 	const virtualLine = await getVirtualLine(id);
+//
+// 	return {
+// 		title: virtualLine.plan.name,
+// 		// openGraph: {
+// 		// 	images: [post.location.cover]
+// 		// },
+// 		other: {
+// 			"apple-itunes-app": "app-id=id6746370915, app-clip-bundle-id=business.xxi.hypesim.Clip"
+// 		}
+// 	}
+// }
 
 export default function Page ({ params }: { params: { id: string } }) {
 	return <h1>Virtual Line {params.id}</h1>
